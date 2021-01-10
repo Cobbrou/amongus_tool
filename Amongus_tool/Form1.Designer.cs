17878738bb08004a476eb42512efda3d8850e328
xninoush <74274043+xninoush@users.noreply.github.com>
2021-01-11 00:50:45 +0100

    ☄ - Added game detection.

--- Amongus_tool/Form1.Designer.cs
+++ Amongus_tool/Form1.Designer.cs
@@ -29,13 +29,57 @@ namespace Amongus_tool
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.BGWorker = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ProcOpenLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BGWorker
+            // 
+            this.BGWorker.WorkerReportsProgress = true;
+            this.BGWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BGWorker_DoWork);
+            this.BGWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.BGWorker_ProgressChanged);
+            this.BGWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BGWorker_RunWorkerCompleted);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(673, 431);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "GAME DETECTED :";
+            // 
+            // ProcOpenLabel
+            // 
+            this.ProcOpenLabel.AutoSize = true;
+            this.ProcOpenLabel.ForeColor = System.Drawing.Color.Red;
+            this.ProcOpenLabel.Location = new System.Drawing.Point(776, 431);
+            this.ProcOpenLabel.Name = "ProcOpenLabel";
+            this.ProcOpenLabel.Size = new System.Drawing.Size(23, 13);
+            this.ProcOpenLabel.TabIndex = 1;
+            this.ProcOpenLabel.Text = "NO";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ProcOpenLabel);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.ComponentModel.BackgroundWorker BGWorker;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label ProcOpenLabel;
     }
 }
 
--- Amongus_tool/Form1.cs
+++ Amongus_tool/Form1.cs
@@ -5,16 +5,52 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Memory;
 
 namespace Amongus_tool
 {
     public partial class Form1 : Form
     {
+        public Mem m = new Mem();
         public Form1()
         {
-            InitializeComponent(); //merde
+            InitializeComponent();
         }
+
+        bool ProcOpen = false;
+
+        private void BGWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (!m.OpenProcess("Among Us"))
+            {
+                Thread.Sleep(1000);
+                return;
+            }
+
+            ProcOpen = true;
+            Thread.Sleep(1000);
+            BGWorker.ReportProgress(0);
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            BGWorker.RunWorkerAsync();
+        }
+
+        private void BGWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if (ProcOpen)
+                ProcOpenLabel.Text = "YES";
+           ProcOpenLabel.ForeColor = Color.FromArgb(40, 193, 0); //GREEN
+        }
+
+        private void BGWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            BGWorker.RunWorkerAsync();
+        }
+
     }
 }
